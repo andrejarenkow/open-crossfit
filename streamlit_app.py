b493cd0bfedf6daa447d42fa219236f3112428d7
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-03-20 13:50:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,13 @@ import streamlit as st
 
 # Configurações da página
 st.set_page_config(
-    page_title="Ovitrampas",
-    page_icon="	:bug:",
+    page_title="Open Crossfit",
+    page_icon="	:weight_lifter:",
     layout="wide",
     initial_sidebar_state='collapsed'
 ) 
 col1, col2, col3 = st.columns([1,4,1])
 
 #col1.image('logo_cevs (1).png', width=200)
-col2.header('Open Crossfit')
+col2.header('Open Crossfit 24')
 #col3.image('logo_estado (3).png', width=300)
