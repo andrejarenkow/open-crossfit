5c934db3c59c64e97df7feb234c4c546876720f3
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-03-20 15:01:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,70 @@ dados['age'] = pd.to_numeric(dados['age'], errors='coerce')
 dados['score_1'] = pd.to_numeric(dados['score_1'], errors='coerce')
 dados['score_2'] = pd.to_numeric(dados['score_2'], errors='coerce')
 
+# Score 24.1
+dados_fizeram_1 = dados[dados['score_1']>0].reset_index(drop=True)
+scores = dados_fizeram_1['scoreDisplay_1'].str.split(' reps', expand=True)[0].str.replace(' - s','').str.replace(' - f','')
+segundos = []
+for i in scores:
+  #print(i)
+  if len(i.split(':'))>1:
+    score = int(i.split(':')[0])*60+int(i.split(':')[1])
+
+  else:
+    score = (180-int(i))*3+15*60
+
+  segundos.append(score)
+
+dados_fizeram_1['score_segundos'] = segundos
+
+# Definindo a função para criar o array que vai no gráfico
+def valores_array_box(nome_box, categoria):
+  scores = pd.Series(dados_fizeram_1[(dados_fizeram_1['affiliateName']==nome_box)&(dados_fizeram_1['scaled_descrito'].isin(categoria))]['score_segundos'], name=nome_box).to_numpy()
+
+  return scores
+
+# Criar uma lista para botar o samples do Ridgeplot
+lista_samples = []
+lista_nomes_afiliados_selecionados = []
+
+for afiliado in lista_afiliados:
+  scores_afiliado = valores_array_box(afiliado, categoria)
+  if len(scores_afiliado)>2:
+    lista_samples.append(scores_afiliado)
+    mediana = int(np.median(scores_afiliado))
+    inscritos = len(scores_afiliado)
+    lista_nomes_afiliados_selecionados.append(f'{afiliado} - Mediana {mediana} segundos - {inscritos} atletas')
+
+
+# Not only does 'ridgeplot(...)' come configured with sensible defaults
+# but is also fully configurable to your own style and preference!
+fig_1 = ridgeplot(
+    samples=lista_samples,
+    #bandwidth=4,
+    kde_points=np.linspace(300, 1300, 500),
+    colorscale="viridis",
+    colormode="row-index",
+    coloralpha=1,
+    labels=lista_nomes_afiliados_selecionados,
+    linewidth=2,
+    spacing=1,
+)
+
+# Again, update the figure layout to your liking here
+fig_1.update_layout(
+    title=f"Comparativo Open 24.1, {categoria}, Porto Alegre, RS",
+    height=650,
+    width=800,
+    plot_bgcolor="rgba(255, 255, 255, 0.0)",
+    xaxis_gridcolor="rgba(0, 0, 0, 0.1)",
+    yaxis_gridcolor="rgba(0, 0, 0, 0.1)",
+    yaxis_title="Nome do box",
+    xaxis_title="Tempo (segundos)",
+    showlegend=False
+)
+
+
+
 # Score 24.2
 dados_fizeram_2 = dados[dados['score_2']>0].reset_index(drop=True)
 scores = dados_fizeram_2['scoreDisplay_2'].str.split(' reps', expand=True)[0].str.replace(' - s','').str.replace(' - f','')
@@ -82,7 +146,7 @@ for afiliado in lista_afiliados:
 
 # Not only does 'ridgeplot(...)' come configured with sensible defaults
 # but is also fully configurable to your own style and preference!
-fig = ridgeplot(
+fig_2 = ridgeplot(
     samples=lista_samples,
     #bandwidth=4,
     kde_points=np.linspace(0, 1300, 500),
@@ -95,7 +159,7 @@ fig = ridgeplot(
 )
 
 # Again, update the figure layout to your liking here
-fig.update_layout(
+fig_2.update_layout(
     title=f"Comparativo Open 24.2, {categoria}, {cidade}",
     height=1000,
     width=800,
@@ -108,4 +172,5 @@ fig.update_layout(
 )
 
 # Show us the work!
-st.plotly_chart(fig)
+st.plotly_chart(fig_1)
+st.plotly_chart(fig_2)
