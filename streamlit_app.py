195cc6a3407d16019b2d568c86e6f25ffa3261d4
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-03-20 17:27:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -170,8 +170,8 @@ fig_2.update_layout(
     height=1000,
     width=400,
     plot_bgcolor="rgba(0, 255, 255, 0.0)",
-    xaxis_gridcolor="rgba(0, 0, 0, 0.1)",
-    yaxis_gridcolor="rgba(0, 0, 0, 0.1)",
+    #xaxis_gridcolor="rgba(0, 0, 0, 0.1)",
+    #yaxis_gridcolor="rgba(0, 0, 0, 0.1)",
     #yaxis_title="Nome do box",
     xaxis_title="Repetições",
     showlegend=False
