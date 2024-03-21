fd336de31e36a15bd7dae99bbba9f15b217d238d
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-03-21 16:28:18 -0300

    Update pagina_inicial.py

--- pagina_inicial.py
+++ pagina_inicial.py
@@ -1,15 +1,6 @@
 import streamlit as st
 
 
-# Configurações da página
-st.set_page_config(
-    page_title="Open Crossfit",
-    page_icon="	:weight_lifter:",
-    layout="wide",
-    initial_sidebar_state='expanded'
-) 
-col1, col2, col3 = st.columns([1,4,1])
-col2.header('Open Crossfit 24')
 
 st.subheader('aaaa')
 
