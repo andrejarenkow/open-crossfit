5fda12901c11132afb3627bda1abe1a968ac0023
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-03-21 15:53:59 -0300

    Update and rename 2_Comparativo.py to Pagina Inicial.py

---
+++ Pagina Inicial.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import plotly.express as px
+import requests
+import numpy as np
+import streamlit as st
+from ridgeplot import ridgeplot
+
+# Configurações da página
+st.set_page_config(
+    page_title="Open Crossfit",
+    page_icon="	:weight_lifter:",
+    layout="wide",
+    initial_sidebar_state='expanded'
+) 
+col1, col2, col3 = st.columns([1,4,1])
+
+
+
+
+col2.header('Open Crossfit 24')
+
+st.subheader('aaaa')
+
